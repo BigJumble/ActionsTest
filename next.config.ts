d0d1611c6366f28846a89ed02adc26de1964fd40
Eimantas <eima.eima1244@gmail.com>
2025-02-16 01:32:22 +0200

    fixing file paths

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output:"export",
   distDir:"out",
-  assetPrefix: isProd ? 'https://bigjumble.github.io/ActionsTest' : 'http://127.0.0.1:5500/out',
+//   assetPrefix: isProd ? 'https://bigjumble.github.io/ActionsTest' : 'http://127.0.0.1:5500/out',
+  basePath: `/${process.env.ROUTE?.trim()}`,
 };
 
 export default nextConfig;
