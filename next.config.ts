ee048776cb7384652c0a7e3ea62d40773066ee8f
Eimantas <eima.eima1244@gmail.com>
2025-02-16 00:23:25 +0200

    export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
+  output:"export",
+  distDir:"out",
+  assetPrefix: isProd ? 'https://bigjumble.github.io/ActionsTest/out' : 'http://127.0.0.1:5500/out',
 };
 
 export default nextConfig;
